34e36c71d3854091a2564b72eec5d27dbcfb4913
Redoudev <redouanee.assal@gmail.com>
2023-04-22 18:16:59 +0200

    Affichage des taches de l'utilisateur connecté

---
+++ TaskPlanner/TaskPlanner/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPlanner.Models
+{
+    // Ce code déclare une classe "Task" avec quatre propriétés:
+    public class Task
+    {
+        // Identifiant unique de la tache
+        public int id_tache { get; set; }
+
+        // Titre de la tache
+        public string titre { get; set; }
+
+        // Nom de la categorie de la tache
+        public string nom_categorie { get; set; }
+
+        // Date echeance de la tache
+        public string date_echeance { get; set; }
+    }
+}
--- TaskPlanner/TaskPlanner/Views/Home.xaml.cs
+++ TaskPlanner/TaskPlanner/Views/Home.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-
+using TaskPlanner.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,60 @@ namespace TaskPlanner.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Home : ContentPage
     {
+        // URL de l'API, avec un paramètre pour l'ID utilisateur
+        private const string apiUrl = "https://xamarinn.alwaysdata.net/taskplanner/controllers/read/readTask.php?id_utilisateur={0}";
+
+        // Instance de la classe HttpClient pour effectuer les requêtes HTTP
+        private HttpClient client = new HttpClient();
+
+        // Liste pour stocker les tâches récupérées depuis l'API
+        private List<Task> tasks = new List<Task>();
+
         public Home()
         {
             InitializeComponent();
+
+            // Appel de la méthode GetTasks pour rafraîchir la liste des tâches lorsqu'on revient sur la page
+            GetTasks();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetTasks();
+        }
+
+        private async void GetTasks()
+        {
+            try
+            {
+                // Récupérer l'ID utilisateur depuis les paramètres globaux
+                int userId = AppSettings.UserId;
+
+                // On appelle l'API pour récupérer les tâches pour cet utilisateur
+                var response = await client.GetAsync(string.Format(apiUrl, userId));
+                response.EnsureSuccessStatusCode();
+
+                // On convertit la réponse en une liste de tâches
+                string json = await response.Content.ReadAsStringAsync();
+                tasks = JsonConvert.DeserializeObject<List<Task>>(json);
+
+                // On affiche les tâches dans la ListView
+                TasksListView.ItemsSource = tasks;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erreur", "Une erreur est survenue : " + ex.Message, "OK");
+            }
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (args.SelectedItem == null)
+                return;
+
+            // définir la couleur de fond sur Transparent
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
